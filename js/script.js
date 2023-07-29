a31611bae5126dae34cd17267d7e725c3c1cb204
Parvina Odinaeva <odinaeva.parvina@gmail.com>
2023-07-30 00:09:00 +0200

    function modification

--- js/script.js
+++ js/script.js
@@ -1,38 +1,28 @@
-
-// button toggling the background
 {
   const backgroundColorToggling = () => {
+
     const body = document.querySelector(".js-body");
     body.classList.toggle("bodyBackgdoundColor");
-  }
+  };
 
-  const init = () => {
-    const button = document.querySelector(".js-bodyBackgdound");
-    button.addEventListener("click", backgroundColorToggling);
-  }
-
-  init();
-}
-
-
-// button hiding and showing an advertisement with a change of the word in the button
-{
 
   const box = document.querySelector(".js-advertisingBox");
-  const showHideWord = document.querySelector(".js-advertisingWord"); 
+  const showHideWord = document.querySelector(".js-advertisingWord");
 
-  const advertisementButton = () => {    
-      if (box.classList.toggle("js-advertisingBox")) {
-        (showHideWord.innerText = "Pokaz");
-      } else {
-        (showHideWord.innerText = "Ukryj");
-      } 
-  }
+  const advertisementToggleBox = () => {
+
+    box.classList.toggle("js-advertisingBox");
+    showHideWord.innerText = box.classList.contains("js-advertisingBox") ? "Pokaż" : "Ukryj";
+  };
 
   const init = () => {
-    const button = document.querySelector(".js-advertisingButton");
-    button.addEventListener("click", advertisementButton);
-  }
+
+    const backgdoundToggleButton = document.querySelector(".js-bodyBackgdound");
+    backgdoundToggleButton.addEventListener("click", backgroundColorToggling);
+
+    const advertisementToggleButton = document.querySelector(".js-advertisingButton");
+    advertisementToggleButton.addEventListener("click", advertisementToggleBox);
+  };
 
   init();
 }
